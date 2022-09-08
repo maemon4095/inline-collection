34c4364f8d8aace7a94fdfe803127f6b189b7f2d
maemon <47819358+maemon4095@users.noreply.github.com>
2022-09-08 10:50:32 +0900

    rename IsSomeAnd to Any

--- src/testing.rs
+++ src/testing.rs
@@ -1,11 +1,11 @@
 #![allow(unused_imports)]
 pub(crate) use memory_testing_utils::*;
 
-pub(crate) trait IsSomeAndExtension<T> {
+pub(crate) trait AnyExtension<T> {
     fn any(&self, predicate: impl FnOnce(&T) -> bool) -> bool;
 }
 
-impl<T> IsSomeAndExtension<T> for Option<T> {
+impl<T> AnyExtension<T> for Option<T> {
     fn any(&self, predicate: impl FnOnce(&T) -> bool) -> bool {
         self.as_ref().map_or(false, predicate)
     }
